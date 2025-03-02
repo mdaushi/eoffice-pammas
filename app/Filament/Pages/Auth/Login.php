bf06b520b017eebd3ef7598231c191acf51a94a2
mdaushi <firdaus.ex19@gmail.com>
2025-03-02 19:25:13 +0800

    refactor: comment out hardcoded credentials in Login.php

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -12,10 +12,10 @@ class Login extends BasePage
     {
         parent::mount();
 
-        $this->form->fill([
-            'email' => 'superadmin@gmail.com',
-            'password' => 'superadmin',
-        ]);
+        // $this->form->fill([
+        //     'email' => 'superadmin@gmail.com',
+        //     'password' => 'superadmin',
+        // ]);
     }
 
     public function form(Form $form): Form
